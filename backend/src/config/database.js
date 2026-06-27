27a28a2e47ad056b4c8cafec90335af879615fda
enrico-rns06 <pdgenrico59@gmail.com>
2026-06-27 18:13:14 -0300

    docs: README do projeto

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,11 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
+const pool = process.env.DATABASE_URL
+    ? new Pool ({
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+    })
+   : new Pool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_NAME,
